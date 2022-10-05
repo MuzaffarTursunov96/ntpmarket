d02bec21e58ffea39f9a9d43fdf739adbff55adb
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-10-05 12:03:58 +0300

    sa

--- projects/models.py
+++ projects/models.py
@@ -50,6 +50,8 @@ class Wishlist(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
+  def __str__(self):
+    return str(self.user.username)
 
 class UserBiddings(models.Model):
   user = models.ForeignKey(User,on_delete=models.PROTECT)
